e2698edae672185d67eb28286a9d3b02c0b94687
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-30 16:02:58 +0100

    set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
-import Card from './Card';
+import Hand from './Hand';
 
 import './App.css';
 
 class App extends Component {
 
+  state = {
+    deck: [],
+  }
+
   componentDidMount(){
-    
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=bonbon&search_simple=1&action=process&page_size=100&json=1`
+    axios.get(url)
+    .then(res => {
+      this.setState({
+      deck: res.data.products,
+    })})
+  }
+
+  drawCard = () =>{
+    const card = Math.floor(Math.random()*100);
+    return this.state.deck.splice(card,1);
   }
 
   render() {
+    const { deck } = this.state;
+    console.log(deck)
     return (
+        this.state.deck.length &&
         <div className="App">
-          <Card />          
+          <Hand 
+            drawCard={this.drawCard}
+          />          
         </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  sweetsDetails: state.search.sweetsDetails,
-});
-
-export default connect(mapStateToProps, { searchSweets })(App);
+export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 
+import './Card.css';
+
 class Card extends Component{
+
   render(){
+    const { image, name, score, sugar, fat } = this.props;
     return(
       <div className='Card'>
-        <h2>Title</h2>
-        <img src='image' alt='title' />
-        <table>
-          <thead>
-            <tr>
-              <td>Caractéristique</td>
-              <td>Valeur</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Taux de sucre</td>
-              <td>100g</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+          <h3>{name}</h3>
+          <div className='card_image' style={{background: `url(${image}) no-repeat top`}}>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <td>Caractéristique</td>
+                <td>Valeur</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Taux de sucre</td>
+                <td>{sugar}</td>
+              </tr>
+              <tr>
+                <td>Taux de graisses</td>
+                <td>{fat}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
     );
   }
 }
