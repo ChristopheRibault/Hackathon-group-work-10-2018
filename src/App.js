11305de937fde9cfc023c6629e0958e815b4955c
ThibltC <yojibo@hotmail.fr>
2018-10-30 23:36:36 +0100

    add damagescalculation

--- src/App.js
+++ src/App.js
@@ -8,10 +8,29 @@ import './App.css';
 
 class App extends Component {
 
+
+
   state = {
     deck: [],
     cardPlayed: {},
     CPUCard: {},
+    CPUPV: 1000,
+    playerPV: 1000,
+
+  }
+
+
+  /**
+   * @author Thibault
+   * @returns {Number} return result of conflict
+   * Calcul Damages 
+   */
+  calculDamage = (attWin, defLoose) => {
+    const result = Math.floor(attWin - defLoose * 4)
+    if (result > 0) {
+      return result
+    }
+    return 0
   }
 
   /**
@@ -19,13 +38,14 @@ class App extends Component {
    * Gets a deck of 100 cards from the API openfoodfacts.
    * The deck is an array containing all the information of every sweet.
    */
-  componentDidMount(){
+  componentDidMount() {
     const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=bonbon&search_simple=1&action=process&page_size=100&json=1`
     axios.get(url)
-    .then(res => {
-      this.setState({
-      deck: res.data.products,
-    })})
+      .then(res => {
+        this.setState({
+          deck: res.data.products,
+        })
+      })
   }
 
   /**
@@ -33,34 +53,45 @@ class App extends Component {
    * @author Christophe
    * @returns {Array} returns the card that has been removed from the deck
    */
-  drawCard = () =>{
-    const drawnCardIndex = Math.floor(Math.random()*this.state.deck.length);
-    return this.state.deck.splice(drawnCardIndex,1);
+  drawCard = () => {
+    const drawnCardIndex = Math.floor(Math.random() * this.state.deck.length);
+    return this.state.deck.splice(drawnCardIndex, 1);
   }
 
-  playCard = (cardProps) =>{
-    console.log(cardProps.indexInHand)
+  playCard = (cardProps) => {
     const CPUCard = this.drawCard()[0];
     this.setState({
       cardPlayed: cardProps,
       CPUCard: CPUCard,
     })
+    if (CPUCard.nutriments.sugars_100g < cardProps.sugar){
+      const result = this.calculDamage(cardProps.sugar,CPUCard.nutriments['saturated-fat_100g'])   
+      this.setState({
+        CPUPV: this.state.CPUPV - result,
+      })
+    } else {
+      const result = this.calculDamage(CPUCard.nutriments.sugars_100g,cardProps.fat)
+      this.setState({
+        playerPV: this.state.playerPV - result,
+      })
+    }
   }
 
   render() {
     return (
-        this.state.deck.length &&
-        <div className="App">
-          <BattleField 
-            playerCardProps={this.state.cardPlayed}
-            CPUCardProps={this.state.CPUCard}
-          />
-          <Hand
-            playCard={this.playCard}
-            drawCard={this.drawCard}
-            playedCardIndex={this.state.cardPlayed.indexInHand}
-          />          
-        </div>
+      this.state.deck.length &&
+      <div className="App">
+
+        <BattleField
+          playerCardProps={this.state.cardPlayed}
+          CPUCardProps={this.state.CPUCard}
+        />
+        <Hand
+          playCard={this.playCard}
+          drawCard={this.drawCard}
+          playedCardIndex={this.state.cardPlayed.indexInHand}
+        />
+      </div>
     );
   }
 }
