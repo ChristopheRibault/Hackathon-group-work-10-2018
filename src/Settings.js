4a4169a073388dbb2236540fda2521adf51abf7b
ChristopheRibault <c.ribault.dev@gmail.com>
2018-10-31 05:06:16 +0100

    little changes

--- src/Settings.js
+++ src/Settings.js
@@ -12,22 +12,28 @@ class Settings extends Component {
         <h1>SUGAR WAR</h1>
         <form>
           <fieldset className='pointsFieldset'>
-            <label htmlFor='initialPoints'>Commencer à {initialPoints} points</label>
+            <label htmlFor='initialPoints'>Resistance de ton foie :<br/> {initialPoints} calories</label>
             <input type='range' id='initialPoints' min='200' max='5000' step='100' value={initialPoints} onChange={handleInitialPointsChange} />
           </fieldset>
           <fieldset>
-            <label htmlFor='playerName'>Nom : </label>
+            <label htmlFor='playerName'>Pseudo : </label>
             <input type='text' id='playerName' onChange={handlePlayerNameChange} value={playerName} />
           </fieldset>
           <fieldset>
             <label htmlFor='avatar'>Avatar : </label>
             <div className='avatarsList'>
               {avatars.map((avatar,i) =>
-                <img id={avatar} onClick={selectAvatar} src={require(`./avatars/${avatar}-ghost.png`)} alt={avatar} key={i}/>
+                <img 
+                  id={avatar} 
+                  onClick={selectAvatar} 
+                  src={require(`./avatars/${avatar}-ghost.png`)} 
+                  alt={avatar} 
+                  key={i}
+                />
               )}
             </div>
           </fieldset>
-          <button onClick={startGame}>Commencer</button>
+          <button onClick={startGame}>A l'attaque</button>
         </form>
       </div>
     );
